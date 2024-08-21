7a280eb0e2a97216fc87930dfb8dd7d5a0ac790e
trun <e.s.trunov@gmail.com>
2024-08-21 18:51:55 +0700

    add brain_games

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,9 +1,9 @@
-from random import randit, choice, random
+from random import randint, choice, random
 
 
 def calc_game():
-    number1 = randit(1, 100), randit(1, 100)
-    number2 = randit(1, 100), randit(1, 100)
+    number1 = randint(1, 100), randint(1, 100)
+    number2 = randint(1, 100), randint(1, 100)
     operators = '+-*'
     operator = random.choice(operators)
     return f'{number1} {operator} {number2}'
